c99246a8c1a63921d37c35f878540ecbf504bb13
eunbi-jeon <22.euneu@gmail.com>
2023-03-07 19:00:44 +0900

    setting

---
+++ src/main/java/com/sobook/SobookApplication.java
@@ -0,0 +1,13 @@
+package com.sobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobook/SobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
